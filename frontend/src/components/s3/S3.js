f8a247c2b1f420eaef2678148e746eee6e0daf15
shivang patel <shivang@nauggets.io>
2023-07-28 17:28:12 +0530

    second commit add Paginantion

--- frontend/src/components/s3/S3.js
+++ frontend/src/components/s3/S3.js
@@ -6,6 +6,8 @@ import { parseString } from "xml2js";
 export const S3=()=>{
 
     const [pics,setPics] = useState([])
+    const [totalData,setTotalData] = useState([])
+    const itemsPerPage = 6
 
     useEffect(()=>{
         let getdata=async()=>{
@@ -16,12 +18,42 @@ export const S3=()=>{
                 let data = JSON.stringify(results)
                 const parseData = JSON.parse(data).ListBucketResult.Contents
                 const images = parseData.map((ele)=>ele.Key[0])
-                setPics(images)
+                setPics(images.slice(0,itemsPerPage))
+                setTotalData(images)
                 });
-           
         }
         getdata()
     },[])
+
+        const handleInfiniteScroll= async()=>{
+            try{
+                if(window.innerHeight + document.documentElement.scrollTop >= document.documentElement.scrollHeight){
+                    const nextPageStart = pics.length;
+                    let nextPageEnd ;
+                    if(nextPageStart+itemsPerPage<=totalData.length){
+                        nextPageEnd = nextPageStart + itemsPerPage;
+                    }
+                    else{
+                        nextPageEnd = nextPageStart + totalData.length - nextPageStart
+                    }
+                    if (nextPageEnd <= totalData.length) {
+                          setPics((prev) => [...prev, ...totalData.slice(nextPageStart, nextPageEnd)]);
+                    } else {
+                      window.removeEventListener('scroll', handleInfiniteScroll);
+                    }
+                 }
+            }
+            catch(e){
+                console.log(e)
+            }
+        }
+
+    useEffect(()=>{
+        window.addEventListener('scroll', handleInfiniteScroll)
+        return () => {
+            window.removeEventListener('scroll', handleInfiniteScroll);
+          };
+    },[pics])
     
 
     return (
